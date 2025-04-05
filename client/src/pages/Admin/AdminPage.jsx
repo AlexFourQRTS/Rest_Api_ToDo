77e8a7be53f6892c7778567dddf443080691eca7
Brahma <xvergox@gmail.com>
2025-04-05 18:41:18 +0300

    Admin User InterFace

--- client/src/pages/Admin/AdminPage.jsx
+++ client/src/pages/Admin/AdminPage.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button, Form, ListGroup, Card } from "react-bootstrap";
 import axios from "axios";
-import UserList from "./UserList";
-import UserTasks from "./UserTasks";
-import UserToDo from "./UserToDo";
-import CreateTaskForm from "./CreateTaskForm";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AdminPage = () => {
   const navigate = useNavigate();
@@ -13,6 +11,17 @@ const AdminPage = () => {
   const [users, setUsers] = useState([]);
   const [selectedUserTasks, setSelectedUserTasks] = useState([]);
   const [selectedUserId, setSelectedUserId] = useState(null);
+  const [editingTask, setEditingTask] = useState(null);
+
+  const [createTaskTitle, setCreateTaskTitle] = useState("");
+  const [createTaskDescription, setCreateTaskDescription] = useState("");
+  const [createTaskStatusTodo, setCreateTaskStatusTodo] = useState("Active Статус");
+  const [createTaskUserId, setCreateTaskUserId] = useState("");
+
+  const [editTaskTitle, setEditTaskTitle] = useState("");
+  const [editTaskDescription, setEditTaskDescription] = useState("");
+  const [editTaskStatusTodo, setEditTaskStatusTodo] = useState("");
+  const [editTaskUserId, setEditTaskUserId] = useState("");
 
   useEffect(() => {
     if (role !== "admin") {
@@ -21,6 +30,21 @@ const AdminPage = () => {
       fetchUsers();
     }
   }, [navigate, role]);
+
+  useEffect(() => {
+    if (users.length > 0 && !createTaskUserId) {
+      setCreateTaskUserId(users[0]?.id);
+    }
+  }, [users, createTaskUserId]);
+
+  useEffect(() => {
+    if (editingTask) {
+      setEditTaskTitle(editingTask.title);
+      setEditTaskDescription(editingTask.description);
+      setEditTaskStatusTodo(editingTask.statusTodo);
+      setEditTaskUserId(editingTask.userId);
+    }
+  }, [editingTask]);
 
   const fetchUsers = async () => {
     try {
@@ -50,42 +74,321 @@ const AdminPage = () => {
     } catch (error) {
       console.error("Ошибка получения задач пользователя:", error);
       setSelectedUserTasks([]);
+      setSelectedUserId(userId);
     }
   };
 
-  const handleUserSelect = (userId) => {
+  const UserSelect = (userId) => {
+    setSelectedUserId(userId);
     fetchUserTasks(userId);
   };
 
-  const handleTaskCreated = () => {
-    if (selectedUserId) {
-      fetchUserTasks(selectedUserId);
+  const CreateTaskSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://skydishch.fun/api/admin/tasksCreate",
+        {
+          title: createTaskTitle,
+          description: createTaskDescription,
+          userId: createTaskUserId,
+          statusTodo: createTaskStatusTodo,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (selectedUserId === createTaskUserId) {
+        fetchUserTasks(selectedUserId);
+      }
+      setCreateTaskTitle("");
+      setCreateTaskDescription("");
+      toast.success(
+        `Задача успешно создана: ${response.data.message || "Задача создана!"}`
+      );
+    } catch (error) {
+      toast.error(
+        `Ошибка создания задачи: ${error.response?.data?.message || "Неизвестная ошибка"}`
+      );
+      console.error("Ошибка создания задачи:", error);
     }
   };
 
+
+  const EditTask = (task) => {
+    setEditingTask(task);
+  };
+
+  const UpdateTask = async () => {
+    if (!editingTask) return;
+
+    try {
+      await axios.put(
+        `https://skydishch.fun/api/admin/tasks/${editingTask.id}`,
+        {
+          id: editingTask.id,
+          title: editTaskTitle,
+          description: editTaskDescription,
+          statusTodo: editTaskStatusTodo,
+          userId: editTaskUserId,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      toast.success("Задача успешно обновлена!");
+      setEditingTask(null);
+      if (selectedUserId) {
+        fetchUserTasks(selectedUserId);
+      }
+    } catch (error) {
+      toast.error(
+        `Ошибка обновления задачи: ${error.response?.data?.message || "Неизвестная ошибка"}`
+      );
+      console.error("Ошибка обновления задачи:", error);
+    }
+  };
+
+  const DeleteTask = async () => {
+    if (!editingTask) return;
+
+    const toastId = toast.warn(
+      <div>
+        <p>Вы уверены, что хотите удалить задачу "{editingTask.title}"?</p>
+        <Button variant="danger" size="sm" onClick={async () => {
+          try {
+            await axios.delete(`https://skydishch.fun/api/admin/tasks/${editingTask.id}`, {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+              },
+            });
+            toast.success("Задача успешно удалена!");
+            setEditingTask(null);
+            if (selectedUserId) {
+              fetchUserTasks(selectedUserId);
+            }
+            toast.dismiss(toastId);
+          } catch (error) {
+            toast.error(
+              `Ошибка удаления задачи: ${error.response?.data?.message || "Неизвестная ошибка"}`
+            );
+            console.error("Ошибка удаления задачи:", error);
+            toast.dismiss(toastId);
+          }
+        }}>
+          Удалить
+        </Button>{' '}
+        <Button variant="secondary" size="sm" onClick={() => toast.dismiss(toastId)}>
+          Отмена
+        </Button>
+      </div>,
+      {
+        position: "top-right",
+        autoClose: false,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      }
+    );
+  };
+
+
+  const [activeWindow, setActiveWindow] = useState('window2');
+
+  const ButtonClick = (windowName) => {
+    setActiveWindow(windowName);
+    if (windowName == "window1") {
+      setEditingTask(null)
+    }
+  };
+
+
+
   return (
     <>
-      <CreateTaskForm users={users} onCreateTask={handleTaskCreated} />
+      <Container fluid>
+        <h2>Административная панель</h2>
 
-      <div className="user-list-task-row">
-        <UserList
-          users={users}
-          onUserSelect={handleUserSelect}
-          selectedUserId={selectedUserId}
-        />
-
-        <div className="todo-admin-check">
-          <UserToDo
-            username={
-              users.find((user) => user.id === selectedUserId)?.username
-            }
-          />
-
-          {selectedUserId && <UserTasks tasks={selectedUserTasks} />}
+        <div>
+          <button className="btn" onClick={() => ButtonClick('window1')}>Создать задачу</button>
+          <button className="btn" onClick={() => ButtonClick('window2')}>Пользователи</button>
         </div>
-      </div>
+
+        {activeWindow === 'window1' && (
+          <div className="mb-4 border p-3">
+            <h3>Создать новую задачу</h3>
+            <Form onSubmit={CreateTaskSubmit}>
+              <Form.Group className="mb-3">
+                <Form.Label className="admin-user">Назначить пользователю</Form.Label>
+                <Form.Select
+                  value={createTaskUserId}
+                  onChange={(e) => setCreateTaskUserId(e.target.value)}
+                >
+                  {users.map((user) => (
+                    <option key={user.id} value={user.id}>
+                      {user.username}
+                    </option>
+                  ))}
+                </Form.Select>
+              </Form.Group>
+
+
+              <Form.Group className="mb-3">
+                <Form.Label className="admin-user">Название задачи</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={createTaskTitle}
+                  onChange={(e) => setCreateTaskTitle(e.target.value)}
+                  required
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label className="admin-user">Описание задачи</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  value={createTaskDescription}
+                  onChange={(e) => setCreateTaskDescription(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label className="admin-user">Статус задачи</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={createTaskStatusTodo}
+                  onChange={(e) => setCreateTaskStatusTodo(e.target.value)}
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Создать задачу
+              </Button>
+            </Form>
+          </div>
+        )}
+
+        {activeWindow === 'window2' && (
+          <Row className="mt-4">
+            <Col md={3} className="user-list-col">
+              <div className="user-list-task-column">
+                {users.map((user) => (
+                  <div
+                    key={user.id}
+                    onClick={() => UserSelect(user.id)}
+                    className={`user-list ${selectedUserId === user.id ? "active" : "1"}`}
+                    style={{
+                      padding: "10px",
+                      borderBottom: "1px solid #ccc",
+                      cursor: "pointer",
+                      backgroundColor: selectedUserId === user.id ? "#f0f0f0" : "transparent",
+                    }}
+                  >
+                    {user.username}
+                  </div>
+                ))}
+              </div>
+            </Col>
+            <Col md={9} className="user-tasks-col">
+              {selectedUserId ? (
+                <>
+                  <div className="d-flex justify-content-between align-items-center mb-2">
+                    <h3>
+                      Задачи пользователя:{" "}
+                      {users.find((user) => user.id === selectedUserId)?.username || `ID ${selectedUserId} || "1"`}
+                    </h3>
+                  </div>
+                  <Card>
+                    <ListGroup variant="flush">
+                      {selectedUserTasks.map((task) => (
+                        <ListGroup.Item key={task.id} className="d-flex justify-content-between align-items-center">
+                          {task.title} - {task.description}
+                          <Button variant="outline-primary" size="sm" onClick={() => EditTask(task)}>
+                            Редактировать
+                          </Button>
+                        </ListGroup.Item>
+                      ))}
+                      {selectedUserTasks.length === 0 && <ListGroup.Item>У пользователя нет задач.</ListGroup.Item>}
+                    </ListGroup>
+                  </Card>
+                </>
+              ) : (
+                <p>Выберите пользователя для просмотра задач.</p>
+              )}
+            </Col>
+          </Row>
+        )}
+
+        {editingTask && (
+          <div className="mb-3 border p-3">
+            <h3>Редактировать задачу</h3>
+            <Form.Group className="mb-3">
+              <Form.Label>Название</Form.Label>
+              <Form.Control
+                type="text"
+                value={editTaskTitle}
+                onChange={(e) => setEditTaskTitle(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Описание</Form.Label>
+              <Form.Control
+                as="textarea"
+                value={editTaskDescription}
+                onChange={(e) => setEditTaskDescription(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Статус</Form.Label>
+              <Form.Control
+                as="select"
+                value={editTaskStatusTodo}
+                onChange={(e) => setEditTaskStatusTodo(e.target.value)}
+              >
+                <option value="Active Статус">Active Статус</option>
+                <option value="В процессе">В процессе</option>
+                <option value="Завершено">Завершено</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Назначить пользователю</Form.Label>
+              <Form.Control
+                as="select"
+                value={editTaskUserId}
+                onChange={(e) => setEditTaskUserId(e.target.value)}
+              >
+                <option value="">Выберите пользователя</option>
+                {users.map((user) => (
+                  <option key={user.id} value={user.id}>
+                    {user.username} (ID: {user.id})
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Button variant="success" onClick={UpdateTask} className="me-2">
+              Сохранить задачу
+            </Button>
+            <Button variant="danger" onClick={DeleteTask}>
+              Удалить задачу
+            </Button>
+          </div>
+        )}
+        <ToastContainer position="top-right" autoClose={500} />
+      </Container>
+
     </>
+
   );
 };
 
 export default AdminPage;
+
+
+
--- client/src/pages/User/UserPage.jsx
+++ client/src/pages/User/UserPage.jsx
@@ -3,19 +3,28 @@ import { useNavigate } from "react-router-dom";
 import { Container, ListGroup, Form, Button } from "react-bootstrap";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
+import { ToastContainer, toast } from "react-toastify";
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const UserPage = () => {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
-  const [tasks, setTasks] = useState([]);
-  const [completedTasks, setCompletedTasks] = useState(new Set());
-  const [page, setPage] = useState(1);
-  const tasksPerPage = 5;
   const userId = token ? jwtDecode(token).userId : null;
+  const username = token ? jwtDecode(token).username : null;
+
+  const [tasks, setTasks] = useState([]);
+  const [editingTaskId, setEditingTaskId] = useState(null);
+  const [editedTask, setEditedTask] = useState({});
+  const [newTask, setNewTask] = useState({
+    title: "",
+    description: "",
+    userId: userId,
+    statusTodo: "Active Статус",
+  });
 
   const fetchTasks = useCallback(async () => {
     if (!userId) return;
-
     try {
       const response = await axios.get(
         `https://skydishch.fun/api/users/${userId}/tasks`,
@@ -38,45 +47,273 @@ const UserPage = () => {
     if (!token) {
       navigate("/login");
     } else {
-      setTasks([]);
-      setPage(1);
-      fetchTasks(true);
+      fetchTasks();
     }
   }, [navigate, token, userId, fetchTasks]);
 
-  const handleTaskCompletion = (taskId) => {
-    const newCompletedTasks = new Set(completedTasks);
-    if (newCompletedTasks.has(taskId)) {
-      newCompletedTasks.delete(taskId);
-    } else {
-      newCompletedTasks.add(taskId);
+  const InputChangeNewTask = (e) => {
+    const { name, value } = e.target;
+    setNewTask((prevTask) => ({ ...prevTask, [name]: value }));
+  };
+
+  const createTask = async () => {
+    if (!newTask.title.trim()) {
+      alert("Пожалуйста, введите название задачи.");
+      return;
     }
-    setCompletedTasks(newCompletedTasks);
+    try {
+      const response = await axios.post(
+        `https://skydishch.fun/api/users/${userId}/tasks`,
+        newTask,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setTasks((prevTasks) => [...prevTasks, response.data]);
+      setNewTask({
+        title: "",
+        description: "",
+        statusTodo: "Active Статус",
+        userId: userId,
+      });
+    } catch (error) {
+      console.error(
+        "Ошибка создания задачи:",
+        error.response?.data || error.message
+      );
+      alert("Не удалось создать задачу.");
+    }
+  };
+
+
+
+  const deleteTask = async (taskId) => {
+    const taskToDelete = tasks.find(task => task.id === taskId);
+    if (!taskToDelete) return;
+
+    const toastId = toast.warn(
+      <div>
+        <p>Вы уверены, что хотите удалить задачу "{taskToDelete.title}"?</p>
+        <Button variant="danger" size="sm" onClick={async () => {
+          try {
+            await axios.delete(
+              `https://skydishch.fun/api/users/${userId}/tasks/${taskId}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+            setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+            toast.success("Задача успешно удалена!");
+            toast.dismiss(toastId);
+          } catch (error) {
+            toast.error(
+              `Ошибка удаления задачи: ${error.response?.data || error.message}`
+            );
+            console.error("Ошибка удаления задачи:", error);
+            fetchTasks(); 
+            toast.dismiss(toastId);
+          }
+        }}>
+          Удалить
+        </Button>{' '}
+        <Button variant="secondary" size="sm" onClick={() => toast.dismiss(toastId)}>
+          Отмена
+        </Button>
+      </div>,
+      {
+        position: "top-right",
+        autoClose: false,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      }
+    );
+  };
+
+
+  const editTask = (task) => {
+    setEditingTaskId(task.id);
+    setEditedTask({ ...task });
+  };
+
+  const InputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedTask((prevTask) => ({
+      ...prevTask,
+      [name]: value,
+    }));
+  };
+
+  const saveTask = async (taskId) => {
+    try {
+      await axios.put(
+        `https://skydishch.fun/api/users/${userId}/tasks/${taskId}`,
+        editedTask,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setTasks((prevTasks) =>
+        prevTasks.map((task) => (task.id === taskId ? { ...editedTask } : task))
+      );
+      setEditingTaskId(null);
+    } catch (error) {
+      console.error(
+        "Ошибка обновления задачи:",
+        error.response?.data || error.message
+      );
+      fetchTasks();
+    }
+  };
+
+  const cancelEdit = () => {
+    setEditingTaskId(null);
+  };
+
+  const [activeWindow, setActiveWindow] = useState('window2');
+
+  const ButtonClick = (windowName) => {
+    setActiveWindow(windowName);
+
   };
 
   return (
     <>
       <br />
-      <h2>Список задач для Id - {userId}</h2>
+      <h2>Пользователь - {username}</h2>
       <br />
+      <div>
+        <button className="btn" onClick={() => ButtonClick('window1')}>Создать</button>
+        <button className="btn" onClick={() => ButtonClick('window2')}>Список</button>
+      </div>
 
-      <Container>
-        <h2>Список задач</h2>
+      {activeWindow === 'window1' && (
 
-        <ListGroup>
-          {tasks.map((task) => (
-            <ListGroup.Item key={task.id}>
-              <Form.Check
-                type="checkbox"
-                label={task.title}
-                checked={completedTasks.has(task.id)}
-                onChange={() => handleTaskCompletion(task.id)}
+
+        <Container className="mb-4">
+
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Название</Form.Label>
+              <Form.Control
+                type="text"
+                name="title"
+                value={newTask.title}
+                onChange={InputChangeNewTask}
               />
-              <p>{task.description}</p>
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-      </Container>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Описание</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="description"
+                value={newTask.description}
+                onChange={InputChangeNewTask}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Control
+                name="userId"
+                value={newTask.userId || userId}
+                readOnly
+              />
+            </Form.Group>
+            <Button variant="primary" onClick={createTask}>
+              Создать задачу
+            </Button>
+          </Form>
+        </Container>
+      )}
+
+      {activeWindow === 'window2' && (
+
+        <Container>
+
+          <ListGroup>
+            {tasks.map((task) => (
+              <ListGroup.Item key={task.id}>
+                {editingTaskId === task.id ? (
+                  <Form>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Название</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="title"
+                        value={editedTask.title || ""}
+                        onChange={InputChange}
+                      />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Описание</Form.Label>
+                      <Form.Control
+                        as="textarea"
+                        name="description"
+                        value={editedTask.description || ""}
+                        onChange={InputChange}
+                      />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Статус</Form.Label>
+                      <Form.Control
+                        as="select"
+                        name="statusTodo"
+                        value={editedTask.statusTodo || ""}
+                        onChange={InputChange}
+                      >
+                        <option value="Active Статус">Active Статус</option>
+                        <option value="В процессе">В процессе</option>
+                        <option value="Завершено">Завершено</option>
+                      </Form.Control>
+                    </Form.Group>
+                    <Button variant="success" onClick={() => saveTask(task.id)}>
+                      Сохранить
+                    </Button>{" "}
+                    <Button variant="secondary" onClick={cancelEdit}>
+                      Отмена
+                    </Button>
+                  </Form>
+                ) : (
+                  <>
+                    <p>{task.description}</p>
+                    <small>Статус: {task.statusTodo}</small>{" "}
+                    <Button
+                      variant="primary"
+                      size="sm"
+                      onClick={() => editTask(task)}
+                    >
+                      Редактировать
+                    </Button>{" "}
+                    <Button
+                      variant="danger"
+                      size="sm"
+                      onClick={() => deleteTask(task.id)}
+                    >
+                      Удалить
+                    </Button>
+                    <br />
+
+                    <div>{task.title}</div>
+
+                    <br />
+                  </>
+                )}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Container>
+      )}
+        <ToastContainer />
     </>
   );
 };
