737b4e2c8dd02114b87d7fe01260a81fb1ea8b24
Maliuk Oleksandr <omaliuk@gerc.ua>
2026-01-30 10:29:05 +0200

    constant

--- client/src/common/constant.js
+++ client/src/common/constant.js
@@ -1,11 +1,4 @@
-
-// export const DOMEN_BRAHMA_CONST = "https://brahmadzen.space"
-// export const REACT_APP_API_URL='http://localhost:5000'
-// export const REACT_APP_EXTERNAL_API_URL='http://localhost:5000/data'
-// export const REACT_APP_ROMSERV_URL='http://localhost:9999/romserv'
-// export const EMUL_URL_CONST='http://localhost:5000/emulator/?core='
-
-export const REACT_APP_API_URL="https://brahmadzen.space"
+export const DOMEN_BRAHMA_CONST = "https://brahmadzen.space"
 export const REACT_APP_EXTERNAL_API_URL="https://brahmadzen.space/data"
-export const REACT_APP_ROMSERV_URL="https://brahmadzen.space/romserv/"
-export const EMUL_URL="https://brahmadzen.space/emulator/?core="
+export const REACT_APP_ROMSERV_URL="https://brahmadzen.space/roms"
+export const EMUL_URL_CONST="https://brahmadzen.space/emulator/?core="
